9b77e0646d606a9c3949091026e0ff1ea3a10383
jashkumar solanki <jashsolanki22@gmail.com>
2024-03-20 11:47:35 +0530

    First Commit

---
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.first;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name;
+    EditText passwoed;
+    EditText email;
+    ImageButton btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        passwoed = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputname = name.getText().toString();
+                String inputpassword = passwoed.getText().toString();
+                String inputemail = email.getText().toString();
+                Toast.makeText(MainActivity.this, "Welcome "+inputname +" to my app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
